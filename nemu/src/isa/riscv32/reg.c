497d613c89f6370febeadddff1c34ac45dc942d8
tracer-ics2023 <tracer@njuics.org>
2024-04-05 17:25:42 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:25:42 up  4:46,  1 user,  load average: 0.22, 0.26, 0.30

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -28,9 +28,9 @@ void isa_reg_display() {
 
   for (i = 0; i < (sizeof(regs) / sizeof(char *)); i++){
     if ((i == 1) || (i == 2) || (i == 3) || (i == 8) || (i == 9)){
-      printf("%-10s\t0x%-8x\t0x%-8x\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
+      printf("%-10s\t0x%-10x\t0x%-10x\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
     } else {
-      printf("%-10s\t0x%-8x\t%-8u\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
+      printf("%-10s\t0x%-10x\t%-10u\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
     }
   }
 }
