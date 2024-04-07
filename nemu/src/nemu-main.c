4f9da2a7a7a8375b23b378c6b9f6727c96587ac2
tracer-ics2023 <tracer@njuics.org>
2024-04-07 11:24:30 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:24:30 up  1:39,  1 user,  load average: 0.47, 0.40, 0.43

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -30,8 +30,6 @@ int main(int argc, char *argv[]) {
   assert(file != NULL);
   while (fgets(line, 65536 + 128, file) != NULL){
     int cnt = sscanf(line, "%u %s", &result, exprbuf);
-    printf("%u\n", result);
-    //Log("%s\n", exprbuf);
     assert(cnt == 2);
     bool success = true;
     word_t ans = expr(exprbuf, &success);
