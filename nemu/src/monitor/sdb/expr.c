f32b0834ddc0dee2958f3c6ddbd4d5b19fab841a
tracer-ics2023 <tracer@njuics.org>
2024-04-09 20:51:05 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:51:05 up  5:26,  1 user,  load average: 1.02, 0.82, 0.82

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -192,7 +192,7 @@ word_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
   mark();
-  //printfExpr(0, nr_token - 1);
+  printfExpr(0, nr_token - 1);
   evalSuccess = true;
   exprAns = eval(0, nr_token - 1, &evalSuccess);
   *success = evalSuccess;
@@ -268,7 +268,7 @@ struct stack_node{
 
 word_t eval(int p, int q, bool *success){
   
-  //printfExpr(p, q);
+  printfExpr(p, q);
   word_t number;
 
   if (p > q){
