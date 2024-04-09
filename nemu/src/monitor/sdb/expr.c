6185f53561f7813da74fa50e06fd694fc3795ef5
tracer-ics2023 <tracer@njuics.org>
2024-04-09 21:02:41 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:02:41 up  5:38,  1 user,  load average: 1.07, 0.70, 0.70

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -176,7 +176,8 @@ void printfExpr(int p, int q, char *express){
         tokens[i].type == TK_POINTER || tokens[i].type == TK_HEX ||
         tokens[i].type == TK_REG || tokens[i].type == TK_NEGATIVE ||
         tokens[i].type == TK_UNEQ || tokens[i].type == TK_EQ){
-        printf("%s", tokens[i].str);
+          printf("%s", tokens[i].str);
+          printf("[%d]", tokens[i].type);
     } else {
         printf("%c", (char)tokens[i].type);
     }
