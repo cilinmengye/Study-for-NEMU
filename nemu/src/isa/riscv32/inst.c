36fe4fb2df5bb3268b20ce60481883dff901d64c
cilinmengye <834430027@qq.com>
2024-04-10 21:32:45 +0800

    PA2 RTFSC(2)

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -29,14 +29,22 @@ enum {
 
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
+/*宏SEXT用于符号扩展*/
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 
+/*
+ * 刚才我们只知道了指令的具体操作(比如auipc是将当前PC值与立即数相加并写入寄存器), 但我们还是不知道操作对象(比如立即数是多少, 写入到哪个寄存器). 为了解决这个问题, 代码需要进行进一步的译码工作, 
+ * 这是通过调用decode_operand()函数来完成的. 这个函数将会根据传入的指令类型type来进行操作数的译码, 
+ * 译码结果将记录到函数参数rd, src1, src2和imm中, 它们分别代表目的操作数的寄存器号码, 两个源操作数和立即数.
+ */
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
+  /*宏BITS用于位抽取*/
   int rs1 = BITS(i, 19, 15);
   int rs2 = BITS(i, 24, 20);
+  /*decode_operand会首先统一对目标操作数进行寄存器操作数的译码, 即调用*rd = BITS(i, 11, 7), 不同的指令类型可以视情况使用rd*/
   *rd     = BITS(i, 11, 7);
   switch (type) {
     case TYPE_I: src1R();          immI(); break;
@@ -57,11 +65,29 @@ static int decode_exec(Decode *s) {
 }
 
   INSTPAT_START();
+  /*INSTPAT(模式字符串, 指令名称, 指令类型, 指令执行操作);
+   * imm is immediate number
+   */
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
+  /*
+   * Mr read the number len of 8 byte from pmem which address is src+imm 
+   * ord_t vaddr_read(vaddr_t addr, int len) 
+   */
   INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
+  /*
+   * Mw write the number len of 8 byte data--src2 to pmem which address is src1+imm 
+   * void vaddr_write(vaddr_t addr, int len, word_t data)
+   */
   INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
-
+  /*#define NEMUTRAP(thispc, code) set_nemu_state(NEMU_END, thispc, code)*/
+  // void set_nemu_state(int state, vaddr_t pc, int halt_ret) {
+  //   difftest_skip_ref();
+  //   nemu_state.state = state;
+  //   nemu_state.halt_pc = pc;
+  //   nemu_state.halt_ret = halt_ret;
+  // }
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+  /*在模式匹配过程的最后有一条inv的规则, 表示"若前面所有的模式匹配规则都无法成功匹配, 则将该指令视为非法指令*/
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
 
