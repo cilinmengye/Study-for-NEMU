c3f2393508830676c61318a6890ce1b5bf5e6644
tracer-ics2023 <tracer@njuics.org>
2024-04-13 21:32:45 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:32:45 up 12:21,  1 user,  load average: 0.34, 0.44, 0.42

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,63 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  assert(out != NULL);
+  assert(fmt != NULL);
+  va_list args;
+  va_start(args, fmt);
+  char *p = out;
+
+  while (*fmt != '\0'){
+    if (*fmt != '%'){
+      *p = *fmt;
+      p++;
+      fmt++;
+      continue;
+    }
+    /*the condition that *fmt == '%'*/
+    fmt++;
+    switch (*fmt)
+    {
+    case 'd':
+      int num = va_arg(args, int);
+      if (num == 0){
+        *p = '0';
+        p++;
+        break;
+      } else if (num < 0){
+        *p = '-';
+        p++;
+        num = -1 * num;
+      }
+      int div = 1;
+      while (num / div >= 10){
+        div *= 10;
+      }
+      while (div > 0){
+        *p = num / div + '0';
+        num %= div;
+        div /= 10;
+      }
+      break;
+    case 's':
+      char *s = va_arg(args, char*);
+      assert(s != NULL);
+      while (*s != '\0'){
+        *p = *s;
+        p++;
+        s++;
+      }
+      break;
+    default:
+      assert(0);
+      break;
+    }
+  }
+  *p = '\0';
+  va_end(args);
+  /*len is not include '\0'*/
+  return p - out - 1;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
