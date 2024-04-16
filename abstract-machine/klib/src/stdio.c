3a447f09a3ccd597aecf69a516e72218b83995cd
tracer-ics2023 <tracer@njuics.org>
2024-04-16 21:27:54 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:27:54 up  7:17,  1 user,  load average: 0.37, 0.58, 0.47

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-#define PARSE_ARGS(...) \
+#define PARSE_ARGS(...) assert(fmt != NULL); \
 va_list args; \
 va_start(args, fmt); \
 int cnt = 0; \
@@ -18,10 +18,31 @@ while (*fmt != '\0'){ \
       continue; \
   } \
   fmt++; \
+  int width = 0; \
+  while (*fmt >= '0' && *fmt <='9'){ \
+    width = width * 10 + (*fmt - '0'); \
+    fmt++; \
+  } \
   switch (*fmt) \
   { \
     case 'd': \
       int num = va_arg(args, int); \
+      int numlen = 0; \
+      if (num == 0){ \
+        numlen = 1; \
+      } else if (num < 0){ \
+        numlen++; \
+      } \
+      int tnum = num; \
+      while (tnum){ \
+        tnum /= 10; \
+        numlen++; \
+      } \
+      while (width - numlen > 0){ \
+         c = ' '; \
+        __VA_ARGS__ \
+        width--; \
+      } \
       if (num == 0){ \
         c = '0'; \
         __VA_ARGS__ \
@@ -47,16 +68,22 @@ while (*fmt != '\0'){ \
       break; \
     case 's': \
       char *s = va_arg(args, char*); \
-      \
+      assert(s != NULL); \
+      int slen = strlen(s); \
+      while (width - slen > 0){ \
+        c = ' '; \
+        __VA_ARGS__ \
+        width--; \
+      } \
       while (*s != '\0'){ \
-        c = *s \
+        c = *s; \
         __VA_ARGS__ \
         cnt++; \
         s++; \
       } \
       break; \
     default: \
-      \
+      assert(0); \
       break; \
   } \
   fmt++; \
