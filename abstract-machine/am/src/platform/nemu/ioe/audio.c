cfaff7d7b11f85e74b3bf9122749ae5aa081aa1e
tracer-ics2023 <tracer@njuics.org>
2024-04-20 18:43:18 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:43:18 up  1:27,  1 user,  load average: 0.62, 0.64, 0.60

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -48,7 +48,7 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   unsigned long last = 0;
   for (int i = 0; i < len; i++){
     unsigned long upt = io_read(AM_TIMER_UPTIME).us / 1000;
-    if (upt - last > 1000 / freq){
+    if ((upt - last)*freq > 1000){
       outb(sbufAddr, *(uint8_t *)(ctl->buf.start + i));
       outl(AUDIO_COUNT_ADDR, io_read(AM_AUDIO_STATUS).count + 1);
       sbufAddr = AUDIO_SBUF_ADDR + io_read(AM_AUDIO_STATUS).count;
