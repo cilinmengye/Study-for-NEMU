74304f97b3a54deae697db37f583dcf3483f5e9e
tracer-ics2023 <tracer@njuics.org>
2024-04-20 19:10:22 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:10:22 up  1:54,  1 user,  load average: 0.84, 0.66, 0.57

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -101,10 +101,6 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
   }
 }
 
-static void audio_sbuf_handler(uint32_t offset, int len, bool is_write){
-  audio_base[reg_count] += 1;
-}
-
 void init_audio() {
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
   audio_base = (uint32_t *)new_space(space_size);
@@ -124,5 +120,5 @@ void init_audio() {
    * 流缓冲区STREAM_BUF是一段MMIO空间, 用于存放来自程序的音频数据, 这些音频数据会在将来写入到SDL库中
    */
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
-  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, audio_sbuf_handler);
+  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
 }
