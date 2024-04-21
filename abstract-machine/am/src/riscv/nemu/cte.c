9b9e9d59ce000303f4a329dacb99bc8b21b5415a
tracer-ics2023 <tracer@njuics.org>
2024-04-21 18:45:32 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:45:32 up  5:40,  1 user,  load average: 1.62, 1.18, 0.77

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -20,6 +20,13 @@ Context* __am_irq_handle(Context *c) {
 
 extern void __am_asm_trap(void);
 
+/*
+ * 用于进行CTE相关的初始化操作. 其中它还接受一个来自操作系统的事件处理回调函数的指针, 
+ * 当发生事件时, CTE将会把事件和相关的上下文作为参数, 来调用这个回调函数, 交由操作系统进行后续处理.
+ * 
+ * 对于riscv32来说, 直接将异常入口地址设置到mtvec寄存器中即可.
+ * cte_init()函数做的第二件事是注册一个事件处理回调函数, 这个回调函数由yield test提供
+ */
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
@@ -34,6 +41,19 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   return NULL;
 }
 
+/*
+ * riscv32提供ecall指令作为自陷指令, 并提供一个mtvec寄存器来存放异常入口地址.
+ * 为了保存程序当前的状态, riscv32提供了一些特殊的系统寄存器, 叫控制状态寄存器(CSR寄存器). 
+ * 在PA中, 我们只使用如下3个CSR寄存器:
+ * mepc寄存器 - 存放触发异常的PC
+ * mstatus寄存器 - 存放处理器的状态
+ * mcause寄存器 - 存放触发异常的原因
+ * 
+ * 用于进行自陷操作, 会触发一个编号为EVENT_YIELD事件. 
+ * 不同的ISA会使用不同的自陷指令来触发自陷操作, 具体实现请RTFSC.
+ * 
+ * riscv32通过mret指令从异常处理过程中返回, 它将根据mepc寄存器恢复PC.
+ */
 void yield() {
 #ifdef __riscv_e
   asm volatile("li a5, -1; ecall");
--- nemu/src/isa/riscv32/local-include/reg.h
+++ nemu/src/isa/riscv32/local-include/reg.h
@@ -17,13 +17,33 @@
 #define __RISCV_REG_H__
 
 #include <common.h>
+#include <isa.h>
+
 
 static inline int check_reg_idx(int idx) {
   IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < MUXDEF(CONFIG_RVE, 16, 32)));
   return idx;
 }
 
+static inline word_t *check_csrs_idx(word_t idx){
+  switch (idx)
+  {
+  case 0x305:
+    return &cpu.csrs.mtvec;
+  case 0x341:
+    return &cpu.csrs.mepc;
+  case 0x300:
+    return &cpu.csrs.mstatus;
+  case 0x342:
+    return &cpu.csrs.mcause;
+  default:
+    assert(0);
+    break;
+  }
+}
+
 #define gpr(idx) (cpu.gpr[check_reg_idx(idx)])
+#define csrs(idx) *(check_csrs_idx(idx))
 
 static inline const char* reg_name(int idx) {
   extern const char* regs[];
