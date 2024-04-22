3dedd52d29f104dcb09a24dcddf46b8c23733a1f
tracer-ics2023 <tracer@njuics.org>
2024-04-22 15:41:59 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:41:59 up  4:06,  1 user,  load average: 1.20, 0.79, 0.45

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:
-      printf("Nanos in yield\n"); break;
+      Log("Nanos in yield\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
