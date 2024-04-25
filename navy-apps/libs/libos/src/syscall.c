d6239ff1fc5780c9031e853651eef7429e8e6c42
tracer-ics2023 <tracer@njuics.org>
2024-04-25 16:24:38 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:24:38 up  3:29,  1 user,  load average: 0.92, 0.70, 0.45

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -84,7 +84,7 @@ int _write(int fd, void *buf, size_t count) {
 extern char end;
 static void *program_break = &end;
 void *_sbrk(intptr_t increment) {
-  int ret = (void *)_syscall_(SYS_brk, (intptr_t)(program_break + increment), 0, 0);
+  int ret = (int)_syscall_(SYS_brk, (intptr_t)(program_break + increment), 0, 0);
   if (ret == 0)
     program_break += increment;
   return (void *)ret;
