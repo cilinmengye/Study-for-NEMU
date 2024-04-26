bdfe6ec8abf6a496dbc5ae16291c4a881e18beee
tracer-ics2023 <tracer@njuics.org>
2024-04-26 10:48:04 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:48:03 up  1:32,  1 user,  load average: 1.23, 0.79, 0.49

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -4,10 +4,12 @@
 int main() {
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
+  printf("success?");
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
+  printf("success?");
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
@@ -15,22 +17,26 @@ int main() {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
+  printf("success?");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
+  printf("success?");
 
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
+  printf("success?");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1 + 1000);
   }
+  printf("success?");
 
   fclose(fp);
 
