7f7f6bf2f595ed9ef58a2a7346bb140ce84f1169
tracer-ics2023 <tracer@njuics.org>
2024-04-26 11:09:04 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:09:04 up  1:53,  1 user,  load average: 1.27, 1.12, 0.85

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -5,13 +5,15 @@
 int main() {
   write(1, "start?\n", 7);
   FILE *fp = fopen("/share/files/num", "r+");
+  write(1, "before success?\n", 16);
   assert(fp);
-  printf("success?");
+  write(1, "success?\n", 9);
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
+  write(1, "before success?\n", 16);
   assert(size == 5000);
-  printf("success?");
+  write(1, "success?\n", 9);
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
@@ -19,26 +21,26 @@ int main() {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-  printf("success?");
+  write(1, "success?\n", 9);
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
-  printf("success?");
+  write(1, "success?\n", 9);
 
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-  printf("success?");
+  write(1, "success?\n", 9);
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1 + 1000);
   }
-  printf("success?");
+  write(1, "success?\n", 9);
 
   fclose(fp);
 
