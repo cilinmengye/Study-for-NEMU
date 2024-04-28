3f4b1dc2257733675de6164a204ec4fbc4a77510
tracer-ics2023 <tracer@njuics.org>
2024-04-28 19:32:56 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:32:56 up  9:27,  1 user,  load average: 0.69, 0.66, 0.80

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -9,11 +9,11 @@ int main() {
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
-  printf("w: %d, h: %d\n", w, h);
-  w = 0;
-  h = 0;
+  // printf("w: %d, h: %d\n", w, h);
+  // w = 0;
+  // h = 0;
   NDL_OpenCanvas(&w, &h);
-  printf("w: %d, h: %d\n", w, h);
+  // printf("w: %d, h: %d\n", w, h);
   NDL_DrawRect(bmp, 0, 0, w, h);
   free(bmp);
   NDL_Quit();
