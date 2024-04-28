20da357325e2fe6a9e389e271e8cbba5f1bf9bf1
tracer-ics2023 <tracer@njuics.org>
2024-04-28 16:17:13 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:17:13 up  6:12,  1 user,  load average: 1.30, 1.19, 0.93

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,7 +28,7 @@ void init_proc() {
   // load program here
   //naive_uload(NULL, "/bin/dummy");
   //naive_uload(NULL, "/bin/event-test");
-  naive_uload(NULL, "");
+  naive_uload(NULL, "/bin/bmp-test");
   //naive_uload(NULL, "/bin/timer-test");
   //naive_uload(NULL, "/bin/file-test");
   //naive_uload(NULL, "/bin/hello");
