ff026961c88d465041232fef7767d9a19fa9c2f0
tracer-ics2023 <tracer@njuics.org>
2024-04-29 14:19:28 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:19:28 up  2:25,  1 user,  load average: 0.87, 1.02, 0.76

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,7 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <assert.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -16,8 +18,28 @@ int SDL_PollEvent(SDL_Event *ev) {
   return 0;
 }
 
+/*
+ * 在miniSDL中实现SDL_WaitEvent(), 它用于等待一个事件. 你需要将NDL中提供的事件封装成SDL事件返回给应用程序
+ * Returns 1 on success or 0 if there was an error while waiting for events
+ */
 int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+  char buf[64];
+  NDL_PollEvent(buf, 64);
+  char keytype[4];
+  char keycode[32];
+  sscanf(buf, "%s %s", keytype, keycode);
+  if (strcmp(keytype, "kd") == 0) event->type = SDL_KEYDOWN;
+  else if (strcmp(keytype, "ku") == 0) event->type =  SDL_KEYUP;
+  else assert(0);
+  for (int i = 0; i < sizeof(keyname) / sizeof(keyname[0]); i++){
+    if (strcmp(keycode, keyname[i]) == 0){
+      event->key.type = event->type;
+      event->key.keysym.sym = i;
+      return 1;
+    }
+  }
+  assert(0);
+  return 0;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
