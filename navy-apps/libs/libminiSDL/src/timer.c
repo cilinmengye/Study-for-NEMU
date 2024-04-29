2e1fb327b2877668828fb060c3d45488edd3092e
tracer-ics2023 <tracer@njuics.org>
2024-04-29 19:25:07 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:25:07 up  7:31,  1 user,  load average: 1.22, 1.02, 0.82

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -10,8 +10,13 @@ int SDL_RemoveTimer(SDL_TimerID id) {
   return 1;
 }
 
+/* 
+ * SDL_GetTicks(): 它和NDL_GetTicks()的功能类似, 但有一个额外的小要求
+ * Returns an unsigned 32-bit value 
+ * representing the number of milliseconds since the SDL library initialized.
+ */
 uint32_t SDL_GetTicks() {
-  return 0;
+  return NDL_GetTicks();
 }
 
 void SDL_Delay(uint32_t ms) {
