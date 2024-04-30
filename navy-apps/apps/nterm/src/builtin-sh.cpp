ebe811e03e9dc8ac52c76dc37d6e549c3dc5146d
tracer-ics2023 <tracer@njuics.org>
2024-04-30 11:43:05 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:43:05 up  2:12,  1 user,  load average: 2.33, 2.02, 1.63

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -12,16 +12,16 @@ static struct {
   const char *description;
   int (*handle)(int, char *);
 } cmd_table [] = {
-  {"nterm", "NTerm (NJU Terminal) NTerm是一个模拟终端, 它实现了终端的基本功能, 包括字符的键入和回退, 以及命令的获取等", cmd_execve}, 
-  {"bmp-test", "一个小的测试程序", cmd_execve},
-  {"hello", "一个小的测试程序", cmd_execve},
-  {"timer-test", "一个小的测试程序", cmd_execve},
-  {"nslider","NSlider (NJU Slider) NSlider是Navy中最简单的可展示应用程序, 它是一个支持翻页的幻灯片播放器", cmd_execve},
-  {"file-test", "一个小的测试程序", cmd_execve},
-  {"event-test", "一个小的测试程序", cmd_execve},
-  {"dummy", "一个小的测试程序", cmd_execve},
-  {"menu", "MENU (开机菜单) 开机菜单是另一个行为比较简单的程序, 它会展示一个菜单, 用户可以选择运行哪一个程序", cmd_execve},
-  {"help", "展示出nterm能够执行的命令", cmd_help},
+  {"nterm", "NTerm (NJU Terminal) NTerm is a simulated terminal that implements the basic functions of the terminal, including character typing and rollback, as well as command acquisition, etc", cmd_execve}, 
+  {"bmp-test", "a small test program", cmd_execve},
+  {"hello", "a small test program", cmd_execve},
+  {"timer-test", "a small test program", cmd_execve},
+  {"nslider","NSlider (NJU Slider) NSlider is the simplest displayable application in Navy. It is a slideshow player that supports page turning", cmd_execve},
+  {"file-test", "a small test program", cmd_execve},
+  {"event-test", "a small test program", cmd_execve},
+  {"dummy", "a small test program", cmd_execve},
+  {"menu", "MENU (Boot Menu) The Boot Menu is another program with a relatively simple behavior. It displays a menu and the user can choose which program to runs", cmd_execve},
+  {"help", "Shows the commands that nterm can execute", cmd_help},
 };
 
 #define NR_CMD sizeof(cmd_table)/sizeof(cmd_table[0])
@@ -42,17 +42,6 @@ static void sh_banner() {
 static void sh_prompt() {
   sh_printf("sh> ");
 }
-
-// static char *init_str(const char *str){
-//   char *ret = (char *)malloc(strlen(str) * sizeof(char));
-//   char *p = ret;
-//   for (int i = 0; i < strlen(str) - 1; i++){
-//     *p = *(str + i);
-//     p++;
-//   }
-//   *p = '\0';
-//   return ret;
-// }
 
 static void sh_handle_cmd(const char *str) {
   char *clstr = (char *)str;
@@ -105,10 +94,8 @@ static int cmd_help(int idx, char *args){
 
   if (arg == NULL) {
     /* no argument given */
-    for (i = 0; i < NR_CMD; i++) {
-      //sh_printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-      sh_printf("%s\n", cmd_table[i].name);
-    }
+    for (i = 0; i < NR_CMD; i++)
+      sh_printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
   }
   else {
     for (i = 0; i < NR_CMD; i++) {
