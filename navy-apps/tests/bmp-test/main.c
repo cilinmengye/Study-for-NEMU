019cb7239781c0b9d6fc14395018108439bb34a4
tracer-ics2023 <tracer@njuics.org>
2024-10-27 21:21:39 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:21:39 up  3:18,  3 users,  load average: 0.43, 0.28, 0.29

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -6,6 +6,17 @@
 #include <SDL.h>
 #include <SDL_bmp.h>
 
+void clean_screen(uint32_t color)
+{
+  int sw, sh;
+  NDL_ScreenWH(&sw, &sh);
+  uint32_t *pixels = malloc(sw * sh * sizeof(uint32_t));
+  for (int i = 0; i < sh; i++)
+	  for (int j = 0; j < sw; j++)
+		  pixels[i * sw + j] = color;
+  NDL_DrawRect(pixels, 0, 0, sw, sh);
+}
+
 int main() {
   NDL_Init(0);
   int w, h;
@@ -15,7 +26,8 @@ int main() {
   // w = 0;
   // h = 0;
   NDL_OpenCanvas(&w, &h);
-  printf("w: %d, h: %d\n", w, h);
+  //printf("w: %d, h: %d\n", w, h);
+  clean_screen(0xffffffff);  
   NDL_DrawRect(bmp, 0, 0, w, h);
   free(bmp);
   NDL_Quit();
