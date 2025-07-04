8bd5ed009710e4eb2374fb783a0011c1ef3295be
tracer-ics2023 <tracer@njuics.org>
2025-07-04 11:03:07 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:03:07 up  1:43,  0 users,  load average: 1.23, 0.64, 0.31

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -113,6 +113,7 @@ char* find_symbol_by_addr(Elf* elf, vaddr_t addr) {
   if (str_data == NULL) Assert(0, "No string table found in ELF file");
   // 计算符号条目数量 = 节大小 / 每条目大小
   sym_count = sym_shdr.sh_size / sym_shdr.sh_entsize;
+  // 因为有可能这个地址并非是函数符号地址，所以需要记录下
   // 在符号表条目中查找地址最接近且不大于 addr 的函数符号
   for (size_t i = 0; i < sym_count; i++) {
     GElf_Sym sym;
