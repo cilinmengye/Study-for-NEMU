4c57dba7a32102c2cd03dbf2dd436ad0bf720075
tracer-ics2023 <tracer@njuics.org>
2025-07-08 17:56:00 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:56:00 up  2:15,  0 users,  load average: 0.05, 0.03, 0.08

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -97,7 +97,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 static void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
     nemu_state.state = NEMU_ABORT;
-    nemu_state.halt_pc = pc;
+    nemu_state.halt_pc = pc; // 这里的PC为当前执行的指令地址
     isa_reg_display();
     Log("DiffTest CheckRegs %s", ANSI_FMT("FLASE", ANSI_FG_RED));
   }
