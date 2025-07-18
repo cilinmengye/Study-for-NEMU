f61c3bfaa76b913ca652b437b67842b6766f4e92
tracer-ics2023 <tracer@njuics.org>
2025-07-18 16:03:10 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:03:10 up 45 min,  1 user,  load average: 0.32, 0.23, 0.25

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -16,7 +16,8 @@
  * If the blit is successful, it returns 0, otherwise it returns -1.
  */
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
-  assert(dst && src);
+  assert(src);
+  assert(dst);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
 
   // 扩展支持调色板
