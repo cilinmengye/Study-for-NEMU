3cf42e2c2d7e2c9bde5107adacda00290e01298c
tracer-ics2023 <tracer@njuics.org>
2025-07-23 16:21:12 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:21:12 up  6:51,  1 user,  load average: 0.50, 0.22, 0.22

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -8,6 +8,11 @@ extern char **environ;
 
 void call_main(uintptr_t *args) {
 
+  uintptr_t sp_val = 0, a0_val = 0;
+  asm volatile ("mv %0, sp" : "=r"(sp_val));
+  asm volatile ("mv %0, a0" : "=r"(a0_val));
+  printf("sp = 0x%lx a0 = 0x%lx\n", (unsigned long)sp_val, (unsigned long)a0_val);
+
   // 按道理来说传参a0 == args == argc的地址
   uintptr_t p = (uintptr_t)args;
   int argc = *(int *)p;
