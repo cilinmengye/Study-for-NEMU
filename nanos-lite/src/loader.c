ee7574771192ed836512a4da1d4e7ef6cfc32ae1
tracer-ics2023 <tracer@njuics.org>
2025-07-23 19:06:08 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:06:08 up  9:36,  1 user,  load average: 0.49, 0.15, 0.05

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -140,7 +140,7 @@ void naive_uload(PCB *pcb, const char *filename) {
     string_addr = (uintptr_t)string_area;
     if (argv[i]) {
       memcpy(ustack_end, &string_addr, plen);
-      string_area += strlen(envp[i]) + 1;
+      string_area += strlen(argv[i]) + 1;
     }
     else memcpy(ustack_end, &zero, plen);
 
