3c53ac697eef5c8009e2203d3768caa30613831f
tracer-ics2023 <tracer@njuics.org>
2025-08-06 20:45:10 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:45:10 up 11:24,  1 user,  load average: 0.88, 0.69, 0.58

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -15,13 +15,14 @@ void call_main(uintptr_t *args) {
 
   // 按道理来说传参a0 == args == argc的地址
   uintptr_t p = (uintptr_t)args;
+  printf("address args: 0x%x\n", p);
   int argc = *(int *)p;
   printf("argc: %d\n", argc);
   char **argv = (char **)(p + 4);
   printf("argv: %s %s\n", argv[0], argv[1]);
   char **envp = (char **)(p + 4 + (argc + 1) * sizeof(uintptr_t));
   printf("envp: %s %s\n", envp[0], envp[1]);
-  
+
   //char *empty[] =  {NULL };
   environ = envp;
 
--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -53,7 +53,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   isa_exec_once(s);
   cpu.pc = s->dnpc;
 
-  if ((uintptr_t)s->pc >= 0x40000000 && (uintptr_t)s->pc <= 0x80000000) {
+  if ((uintptr_t)s->pc >= 0x40000000 && (uintptr_t)s->pc < 0x80000000) {
     // Log("0x%x: %x", s->pc, s->isa.inst.val);
     // if ((uintptr_t)s->pc == 0x40008eb0) Log("pc: 0x40008eb0 call main");
     urgent_log_write("0x%x: %x\n", s->pc, s->isa.inst.val);
