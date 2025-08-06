4e28e35a97fc0dd718c7267ec987b29484561132
tracer-ics2023 <tracer@njuics.org>
2025-08-06 19:46:19 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:46:19 up 10:25,  1 user,  load average: 1.65, 0.98, 0.60

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -53,10 +53,11 @@ static void exec_once(Decode *s, vaddr_t pc) {
   isa_exec_once(s);
   cpu.pc = s->dnpc;
 
-  if ((uintptr_t)s->pc >= 0x40000000 && (uintptr_t)s->pc <= 0x80000000) {
-    Log("0x%x: %x\n", s->pc, s->isa.inst.val);
-    //Log("pc: 0x%x", s->pc);
-  }
+  log_write("%s\n", "1");
+  // if ((uintptr_t)s->pc >= 0x40000000 && (uintptr_t)s->pc <= 0x80000000) {
+  //   Log("0x%x: %x\n", s->pc, s->isa.inst.val);
+  //   //Log("pc: 0x%x", s->pc);
+  // }
 #ifdef CONFIG_ITRACE  
   char *p = s->logbuf;
   /* 
