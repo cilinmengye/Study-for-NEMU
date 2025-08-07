420e18103f10281606efe73af339199c23d0d825
tracer-ics2023 <tracer@njuics.org>
2025-08-07 20:24:20 +0800

    >  compile NEMU 834430027lin@gmail.com 次林梦叶 Linux cilinmengye 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:24:20 up 1 day, 11:03,  1 user,  load average: 0.90, 0.77, 0.95

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -66,8 +66,10 @@ void init_proc() {
   // char *const argv[] = {"/bin/nterm", NULL};
   // char *const envp[] = {NULL};
   // context_uload(&pcb[1], "/bin/nterm", argv, envp);
-  char *const argv[] = {"/bin/pal", NULL};
-  char *const envp[] = {NULL};
+  // char *const argv[] = {"/bin/pal", NULL};
+  // char *const envp[] = {NULL};
+  char *const argv[] = {"/bin/pal", "a", "b", NULL};
+  char *const envp[] = {"HOME=/home/cilinmengye", "USER=cilinmengye", NULL};
   context_uload(&pcb[1], "/bin/pal", argv, envp);
 
   // char *const argv[] = {"/bin/dummy", NULL};
